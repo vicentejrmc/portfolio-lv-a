97c8cd171d807e8792b0d6353b3d57aa81b371d5
Vicente Junior <vicente_pc@outlook.com>
2025-10-05 22:59:46 -0300

    implementada sessão de habilidades

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,38 @@
 import { Component } from '@angular/core';
 
+interface ItemHabilidade{
+  icone: string;
+  titulo: string;
+  descricao?: string;
+}
+
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  public itensHabilidades: ItemHabilidade[] = [
+    {
+      icone: 'bi-window-fullscreen',
+      titulo: 'Desenvolvimento Web',
+      descricao: `Design e desenvolvimento de sistemas web atraentes e responsivos.`,
+    },
+    {
+      icone: 'bi-server',
+      titulo: 'Aplicações Back-End',
+      descricao: `Desenvolvimento de sistemas robustos para solucionar problemas simples ou complexos.`,
+    },
+    {
+      icone: 'bi-graph-up',
+      titulo: 'Testes Automatizados',
+      descricao: `Criação de testes automatizados que facilitam a manutenção e melhoram as entregas das aplicações.`,
+    },
+    {
+      icone: 'bi-building-up',
+      titulo: 'Implantação',
+      descricao: `Implantação e manutenção de sistemas em servidores VPS e em nuvem.`,
+    },
+  ];
+}
+
