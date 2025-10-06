16de1204f698bb5825800178e1633c93f0c263fd
Vicente Junior <vicente_pc@outlook.com>
2025-10-05 23:38:17 -0300

    Implementadao sessão minhas habilidades - skill-icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -34,5 +34,96 @@ export class App {
       descricao: `Implantação e manutenção de sistemas em servidores VPS e em nuvem.`,
     },
   ];
+
+  public itensHabildades2: ItemHabilidade[] = [
+     {
+      icone: 'https://skillicons.dev/icons?i=html&theme=dark',
+      titulo: 'HTML',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=css&theme=dark',
+      titulo: 'CSS',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=scss&theme=dark',
+      titulo: 'SCSS',
+    },
+    // {
+    //   icone: 'https://skillicons.dev/icons?i=js&theme=dark',
+    //   titulo: 'JavaScript',
+    // },
+    {
+      icone: 'https://skillicons.dev/icons?i=ts&theme=dark',
+      titulo: 'TypeScript',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=bootstrap&theme=dark',
+      titulo: 'Bootstrap',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=angular&theme=dark',
+      titulo: 'Angular',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=rxjs&theme=dark',
+      titulo: 'RxJS',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=net&theme=dark',
+      titulo: '.NET',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=cs&theme=dark',
+      titulo: 'C#',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=selenium&theme=dark',
+      titulo: 'Selenium',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=cypress&theme=dark',
+      titulo: 'Cypress',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=sqlite&theme=dark',
+      titulo: 'SQLite',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=postgres&theme=dark',
+      titulo: 'Postgres',
+    },
+    // {
+    //   icone: 'https://skillicons.dev/icons?i=redis&theme=dark',
+    //   titulo: 'Redis',
+    // },
+    {
+      icone: 'https://skillicons.dev/icons?i=docker&theme=dark',
+      titulo: 'Docker',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=powershell&theme=dark',
+      titulo: 'PowerShell',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=git&theme=dark',
+      titulo: 'Git',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=github&theme=dark',
+      titulo: 'GitHub',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=azure&theme=dark',
+      titulo: 'Azure',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=visualstudio&theme=dark',
+      titulo: 'Visual Studio',
+    },
+    {
+      icone: 'https://skillicons.dev/icons?i=vscode&theme=dark',
+      titulo: 'VSCode',
+    },
+  ];
 }
 
